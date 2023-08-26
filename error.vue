03c7cdb9a0d860977ba4625c544d5728fb2c75bf
Sammy Aknan <76685474+Sammster10@users.noreply.github.com>
2023-08-25 20:27:55 -0400

    error page

---
+++ error.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <BasicTextContent class="txt-center">
+    <h1 class="top-pad-3">Uh Oh, Something Went Wrong!</h1>
+    <h3 class="inline-pad-3">There was an error with the page that you requested. Make sure that you didn't misspell anything in the URL!</h3>
+
+  </BasicTextContent>
+</template>
+
+<style scoped>
+h1 {
+  color: #de1414;
+  font-size: 4rem;
+  margin: 0;
+}
+
+h3 {
+  color: #424242;
+  font-size: 1.2rem;
+  font-weight: 400;
+  margin: 0;
+}
+</style>
