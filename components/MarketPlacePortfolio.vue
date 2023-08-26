b5718501360a25e2768c006d14c63c4f0a7f82b6
Sammy Aknan <76685474+Sammster10@users.noreply.github.com>
2023-08-25 20:15:50 -0400

    fixed formatting issue in marketplace portfolio

--- components/MarketPlacePortfolio.vue
+++ components/MarketPlacePortfolio.vue
@@ -9,7 +9,7 @@ import ExpandableImage from "~/components/ExpandableImage.vue";
   <PortfolioDisplay>
     <div class="item">
       <div class="description">
-        <h3><span class="tooltip">SkyBlock Game-mode<span class="tooltiptext">SkyBlock: Blocks And Biomes</span></span> <LinkIcon to="https://www.minecraft.net/en-us/marketplace/pdp?id=d0046fbd-1771-4f98-8783-fcd2adb0ef13" /></h3>
+        <h3><span class="tooltip">SkyBlock Game-mode<span class="tooltiptext">SkyBlock: Blocks And Biomes</span></span><LinkIcon to="https://www.minecraft.net/en-us/marketplace/pdp?id=d0046fbd-1771-4f98-8783-fcd2adb0ef13" /></h3>
         <hr>
         <p>An <span class="tooltip">MCBE<span class="tooltiptext">Minecraft Bedrock Edition</span></span> SkyBlock game-mode which is available for purchase on the Minecraft Marketplace. Check it out on <a href="https://youtu.be/-hBCJksz_Vk" target="_blank">YouTube</a>!</p>
       </div>
@@ -17,7 +17,7 @@ import ExpandableImage from "~/components/ExpandableImage.vue";
     </div>
     <div class="item">
       <div class="description">
-        <h3>Capture The Flag <LinkIcon to="https://www.minecraft.net/en-us/marketplace/pdp?id=b91ac1f3-1b80-492d-9a50-9f7c26fe14ca" /></h3>
+        <h3>Capture The Flag<LinkIcon to="https://www.minecraft.net/en-us/marketplace/pdp?id=b91ac1f3-1b80-492d-9a50-9f7c26fe14ca" /></h3>
         <hr>
         <p>Two teams go head to head, trying to destroy one another, while protecting themselves. This is available for purchase.</p>
       </div>
